d60e2a2a9aac71d2b916fc1cb9bc9cdd86ccc989
Tanay Tibrewal <archanatanay17@gmail.com>
2024-10-31 01:41:07 +0530

    Everything working

--- README.md
+++ README.md
@@ -1,85 +1,45 @@
-# Real-time Election Voting System 🗳️
+1. Create the below files and folders:
+.env
+and 1 checkpoint folder with 3 subfolders:
+```bash
+mkdir checkpoints checkpoints/checkpoint1 checkpoints/checkpoint2 checkpoints/checkpoint3
 
-This repository contains the code for a real-time election voting system developed to address concerns about election integrity and transparency. In response to allegations of a rigged voting system favoring the ruling party BJP in India, this system was designed to ensure a fair and transparent electoral process.
+touch .env
+```
+and make changes to env using the .env.sample
 
-## System Architecture 🏗️
-![system_architecture.jpg](images%2Fsystem_architecture.jpg)
-
-## System Flow 🔄
-![system_flow.jpg](images%2Fsystem_flow.jpg)
-
-## System Components 🧩
-- **main.py**: Creates the required tables in Postgres (`candidates`, `voters`, and `votes`), creates the Kafka topic, and replicates the `votes` table in the Kafka topic. It also contains the logic to consume votes from the Kafka topic and produce data to `voters_topic` on Kafka.
-- **voting.py**: Contains the logic to consume votes from the Kafka topic (`voters_topic`), generate voting data, and produce data to `votes_topic` on Kafka.
-- **spark-streaming.py**: Contains the logic to consume votes from the Kafka topic (`votes_topic`), enrich the data from Postgres, aggregate the votes, and produce data to specific topics on Kafka.
-- **streamlit-app.py**: Contains the logic to consume the aggregated voting data from the Kafka topic and Postgres, displaying the voting data in real-time using Streamlit.
-
-## Setting Up the System 🛠️
-
-Follow these steps to set up the real-time election voting system:
-
-1. **Clone the Repository**:
-   ```bash
-   git clone https://github.com/yourusername/realtime-election-voting-system.git
-   cd realtime-election-voting-system
-
-This Docker Compose file allows you to easily spin up Zookkeeper, Kafka and Postgres application in Docker containers. 
-
-### Prerequisites
-- Python 3.9 or above installed on your machine
-- Docker Compose installed on your machine
-- Docker installed on your machine
-
-
-### Steps to Run
-1. Clone this repository.
-2. Navigate to the root containing the Docker Compose file.
-3. Run the following command:
-
+2. Start containers
 ```bash
 docker-compose up -d
 ```
-This command will start Zookeeper, Kafka and Postgres containers in detached mode (`-d` flag). Kafka will be accessible at `localhost:9092` and Postgres at `localhost:5432`.
-
-##### Additional Configuration
-If you need to modify Zookeeper configurations or change the exposed port, you can update the `docker-compose.yml` file according to your requirements.
-
-### Running the App
-1. Install the required Python packages using the following command:
-
+3. Start Python virtual env
+```bash
+python3 -m venv venv 
+source venv/bin/activate 
+```
+4. Install packages
 ```bash
 pip install -r requirements.txt
 ```
 
-2. Creating the required tables on Postgres and generating voter information on Kafka topic:
-
+5. Create Postgres tables and randomly generate candidates/voter info.
 ```bash
 python main.py
 ```
 
-3. Consuming the voter information from Kafka topic, generating voting data and producing data to Kafka topic:
-
+6. Randomly generate votes and produce them to Kafka topic
 ```bash
 python voting.py
 ```
 
-4. Consuming the voting data from Kafka topic, enriching the data from Postgres and producing data to specific topics on Kafka:
-
+7. Consume vote data from Kafka topic and process data with Spark
 ```bash
-python spark-streaming.py
+python spark.py
 ```
 
-5. Running the Streamlit app:
-
+8. Run Streamlit app
 ```bash
+source venv/bin/activate 
+
 streamlit run streamlit-app.py
 ```
-
-## Screenshots
-
-### Voters
-![voters.png](images%2Fvoters.png)
-
-### Voting
-![voting.png](images%2Fvoting.png)
-
--- spark-streaming.py
+++ spark-streaming.py
@@ -1,102 +1,139 @@
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import from_json, col, lit
 from pyspark.sql.functions import sum as _sum
-from pyspark.sql.types import StructType, StructField, StringType, IntegerType, TimestampType
+from pyspark.sql.types import (
+    StructType,
+    StructField,
+    StringType,
+    IntegerType,
+    TimestampType,
+)
+import os
+from dotenv import load_dotenv
+# loading variables from .env file
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
+    load_dotenv()
     # init spark session
-    spark = (SparkSession.builder.appName('RealtimeVoting')
-             .config('spark.jars.packages', 'org.apache.spark:spark-sql-kafka-0-10_2.12:3.5.0') # spark kafka integration
-             .config('spark.jars', '/workspaces/RT_Voting/postgresql-42.7.1.jar') # postgres driver
-             .config('spark.sql.adaptive.enable', 'false') # disable adaptive query execution
-             .getOrCreate())
-    
-    vote_schema = StructType([
-        StructField("voter_id", StringType(), True),
-        StructField("candidate_id", StringType(), True),
-        StructField("voting_time", TimestampType(), True),
-        StructField("voter_name", StringType(), True),
-        StructField("party_affiliation", StringType(), True),
-        StructField("biography", StringType(), True),
-        StructField("campaign_platform", StringType(), True),
-        StructField("photo_url", StringType(), True),
-        StructField("candidate_name", StringType(), True),
-        StructField("date_of_birth", StringType(), True),
-        StructField("gender", StringType(), True),
-        StructField("nationality", StringType(), True),
-        StructField("registration_number", StringType(), True),
-        StructField("address", StructType([
-            StructField("street", StringType(), True),
-            StructField("city", StringType(), True),
-            StructField("state", StringType(), True),
-            StructField("country", StringType(), True),
-            StructField("postcode", StringType(), True)
-        ]), True),
-        StructField("email", StringType(), True),
-        StructField("phone_number", StringType(), True),
-        StructField("cell_number", StringType(), True),
-        StructField("picture", StringType(), True),
-        StructField("registered_age", IntegerType(), True),
-        StructField("vote", IntegerType(), True)
-    ])
+    spark = (
+        SparkSession.builder.appName("RealtimeVoting")
+        .config(
+            "spark.jars.packages", "org.apache.spark:spark-sql-kafka-0-10_2.12:3.5.0"
+        )  # spark kafka integration
+        .config(
+            "spark.jars", os.getenv('JAR_PATH')
+        )  # postgres driver
+        .config(
+            "spark.sql.adaptive.enable", "false"
+        )  # disable adaptive query execution
+        .getOrCreate()
+    )
 
-    votes_df = (spark.readStream
-                .format('kafka')
-                .option('kafka.bootstrap.servers', 'localhost:9092')
-                .option('subscribe', 'votes_topic')
-                .option('startingOffsets', 'earliest')
-                .load()
-                .selectExpr('CAST(value AS STRING)')
-                .select(from_json(col('value'), vote_schema).alias('data'))
-                .select('data.*'))
+    vote_schema = StructType(
+        [
+            StructField("voter_id", StringType(), True),
+            StructField("candidate_id", StringType(), True),
+            StructField("voting_time", TimestampType(), True),
+            StructField("voter_name", StringType(), True),
+            StructField("party_affiliation", StringType(), True),
+            StructField("biography", StringType(), True),
+            StructField("campaign_platform", StringType(), True),
+            StructField("photo_url", StringType(), True),
+            StructField("candidate_name", StringType(), True),
+            StructField("date_of_birth", StringType(), True),
+            StructField("gender", StringType(), True),
+            StructField("nationality", StringType(), True),
+            StructField("registration_number", StringType(), True),
+            StructField(
+                "address",
+                StructType(
+                    [
+                        StructField("street", StringType(), True),
+                        StructField("city", StringType(), True),
+                        StructField("state", StringType(), True),
+                        StructField("country", StringType(), True),
+                        StructField("postcode", StringType(), True),
+                    ]
+                ),
+                True,
+            ),
+            StructField("email", StringType(), True),
+            StructField("phone_number", StringType(), True),
+            StructField("cell_number", StringType(), True),
+            StructField("picture", StringType(), True),
+            StructField("registered_age", IntegerType(), True),
+            StructField("vote", IntegerType(), True),
+        ]
+    )
+
+    votes_df = (
+        spark.readStream.format("kafka")
+        .option("kafka.bootstrap.servers", "localhost:9092")
+        .option("subscribe", "votes_topic")
+        .option("startingOffsets", "earliest")
+        .load()
+        .selectExpr("CAST(value AS STRING)")
+        .select(from_json(col("value"), vote_schema).alias("data"))
+        .select("data.*")
+    )
 
     # data preprocessing: typecasting and watermarking
-    votes_df = votes_df.withColumn('voting_time', col('voting_time').cast(TimestampType())).withColumn('vote', col('vote').cast(IntegerType()))
+    votes_df = votes_df.withColumn(
+        "voting_time", col("voting_time").cast(TimestampType())
+    ).withColumn("vote", col("vote").cast(IntegerType()))
     # discard any event that is processed later than 1 minute after voting time
-    enriched_votes_df = votes_df.withWatermark('voting_time', '1 minute')
+    enriched_votes_df = votes_df.withWatermark("voting_time", "1 minute")
 
     # The resulting votes_per_candidate DataFrame will have a schema with columns 'candidate_id', 'candidate_name', 'party', 'photo_url', and 'total_votes'.
-    votes_per_candidate = enriched_votes_df.groupBy('candidate_id', 'candidate_name', 'party_affiliation', 'photo_url').agg(_sum('vote').alias('total_votes'))
+    votes_per_candidate = enriched_votes_df.groupBy(
+        "candidate_id", "candidate_name", "party_affiliation", "photo_url"
+    ).agg(_sum("vote").alias("total_votes"))
 
-    turnout_by_location = enriched_votes_df.groupBy('address.state').count().alias('total_votes')
+    turnout_by_location = (
+        enriched_votes_df.groupBy("address.state").count().alias("total_votes")
+    )
 
-    total_votes = enriched_votes_df.agg(_sum('vote').alias('total_votes'))
+    total_votes = enriched_votes_df.agg(_sum("vote").alias("total_votes"))
 
-    total_votes_to_kafka = (total_votes.selectExpr('to_json(struct(*)) AS value')
-                            .writeStream
-                            .format('kafka')
-                            .option('kafka.bootstrap.servers', 'localhost:9092')
-                            .option('topic', 'aggregated_total_votes')
-                            .option('checkpointLocation', '/workspaces/RT_Voting/checkpoints/checkpoint1')
-                            .outputMode('update')
-                            .start())
+    total_votes_to_kafka = (
+        total_votes.selectExpr("to_json(struct(*)) AS value")
+        .writeStream.format("kafka")
+        .option("kafka.bootstrap.servers", "localhost:9092")
+        .option("topic", "aggregated_total_votes")
+        .option("checkpointLocation", os.getenv('CHECKPOINT1'))
+        .outputMode("update")
+        .start()
+    )
 
-    votes_per_candidate_to_kafka = (votes_per_candidate.selectExpr('to_json(struct(*)) AS value') # The result is a DataFrame with a single column named 'value' containing JSON-encoded strings.
-                                    .writeStream
-                                    .format('kafka')
-                                    .option('kafka.bootstrap.servers', 'localhost:9092')
-                                    .option('topic', 'aggregated_votes_per_candidate')
-                                    .option('checkpointLocation', '/workspaces/RT_Voting/checkpoints/checkpoint2')
-                                    .outputMode('update')
-                                    .start())
-    
-    turnout_by_location_to_kafka = (turnout_by_location.selectExpr('to_json(struct(*)) AS value')
-                                    .writeStream
-                                    .format('kafka')
-                                    .option('kafka.bootstrap.servers', 'localhost:9092')
-                                    .option('topic', 'aggregated_turnout_by_location')
-                                    .option('checkpointLocation', '/workspaces/RT_Voting/checkpoints/checkpoint3')
-                                    .outputMode('update')
-                                    .start())
-    
+    votes_per_candidate_to_kafka = (
+        votes_per_candidate.selectExpr(
+            "to_json(struct(*)) AS value"
+        )  # The result is a DataFrame with a single column named 'value' containing JSON-encoded strings.
+        .writeStream.format("kafka")
+        .option("kafka.bootstrap.servers", "localhost:9092")
+        .option("topic", "aggregated_votes_per_candidate")
+        .option("checkpointLocation", os.getenv('CHECKPOINT2'))
+        .outputMode("update")
+        .start()
+    )
+
+    turnout_by_location_to_kafka = (
+        turnout_by_location.selectExpr("to_json(struct(*)) AS value")
+        .writeStream.format("kafka")
+        .option("kafka.bootstrap.servers", "localhost:9092")
+        .option("topic", "aggregated_turnout_by_location")
+        .option("checkpointLocation", os.getenv('CHECKPOINT3'))
+        .outputMode("update")
+        .start()
+    )
+
     # blocks current thread until streaming ends
     votes_per_candidate_to_kafka.awaitTermination()
     turnout_by_location_to_kafka.awaitTermination()
     total_votes_to_kafka.awaitTermination()
 
 
-'''
+"""
 In Spark Structured Streaming, the defined operations such as watermarking, aggregation, and other transformations are part of a query plan. 
 When you express these operations in your Spark Structured Streaming code, Spark internally builds a query plan based on the transformations you've specified. 
 This plan represents the logical flow of data processing.
@@ -134,4 +171,4 @@ Here's a simplified overview of how Spark Structured Streaming works with these 
 
 By combining these elements, Spark Structured Streaming creates a continuous data processing pipeline that incrementally updates state, handles late-arriving data through watermarking, 
 and ensures fault tolerance through periodic checkpointing. This design enables Spark to efficiently process and update results as new data arrives in the streaming source.
-'''
+"""
